b286c80cff0f65279f20bf9cd52dc17146b89497
unknown <vojtpavlo3@gmail.com>
2020-09-25 17:25:13 +0300

    homework1

---
+++ homework1/index.js
@@ -0,0 +1,18 @@
+// const name = 'Генератор захисного поля' ;
+// const price = '1000' ; 
+// const  verse =` Обрано ${name}, ціна за штуку ${price} кредитів` ;
+// const ref = document.querySelector(".q");
+// ref.textContent = verse;  ----це просто складніший варіант але не на консоль а просто на сайт зразy
+
+
+let name = (" Генератор захисного поля" );
+var price =  ' 1000' ;
+
+let text = (" Обрано")  ;
+
+let mw=(" ціна  за  штуку") ; 
+let r = (" кредитів");
+var price =  ' 2000' ;
+console.log( text + name +  mw + price +  r )
+ 
+ 
