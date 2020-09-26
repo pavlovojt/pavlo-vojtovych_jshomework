8d79217cc6fa59e553e8fc35478398b7ae21e843
unknown <vojtpavlo3@gmail.com>
2020-09-26 18:23:57 +0300

    Homework2

---
+++ HW2/Завдання2/index.js
@@ -0,0 +1,12 @@
+const total = 100;
+let ordered = 50;
+
+
+ordered = 20;
+ordered = 80;
+ordered = 130;
+let q = ordered < total ? "На складі недостатньо товарів!" : "Замовлення оформлено, з вами зв'яжеться менеджер";
+console.log(q);
+
+
+
---
+++ HW2/Завдання3/index.js
@@ -0,0 +1,8 @@
+
+const message = prompt("Password");
+if(message === null){alert("Відмінено користувачем")}
+if(message == "jqueryismyjam"){alert("Ласкаво просимо")}else{alert("Пароль хибний")};
+
+
+
+
