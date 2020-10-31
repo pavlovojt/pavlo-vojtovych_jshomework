d591f082ee7019926bc4b3c8b611f0fef8cbf1eb
unknown <vojtpavlo3@gmail.com>
2020-10-31 16:09:28 +0200

    HW6

--- hw6/index.js
+++ hw6/index.js
@@ -1,19 +1,24 @@
-
-
-const findLongestWord = function(string) {
-  return string.split(' ').reduce((a, b) => (b.length > a.length) ? b : a);
+const findLongestWord = function (string) {
+  let str =  string.split(" ");
+  let longest = 0;
+  let word = null;
+  for (let i = 0; i<str.length;  i += 1) {
+    if ( longest < str[i].length) {
+      longest =  str[i].length;
+    word = str[i]
+  }
+  }
+  return  word;
 };
 
+/*
+ * Виклич функції для перевірки працездатності твоєї реалізації.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
 
+console.log(findLongestWord('Google do a roll')); // 'Google'
 
-console.log(
-  findLongestWord("The quick brown fox jumped over the lazy dog")
-); 
+console.log(findLongestWord('May the force be with you')); // 'force'
 
-console.log(
-  findLongestWord(" Google    do a roll  ")
-); 
+  
 
-console.log(
-    findLongestWord("May the force be with you")
-  );      
