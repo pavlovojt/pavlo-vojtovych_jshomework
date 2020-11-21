f09933b1932f30b4ebd82b9cae2d07da3c87f72e
unknown <vojtpavlo3@gmail.com>
2020-11-21 14:47:36 +0200

    HW8

---
+++ hw8/index.js
@@ -0,0 +1,7 @@
+const countProps = function (obj) 
+{let total = 0; 
+    for (const key in obj) { total += 1;}
+    return total ;};
+ console.log(countProps({}));
+  console.log(countProps({ name: 'Mango', age: 2 })); 
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
