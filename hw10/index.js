17ab5c71caba96c6ad06ffe2ac781134e1263570
unknown <vojtpavlo3@gmail.com>
2020-11-28 15:25:52 +0200

    HW10

---
+++ hw10/index.js
@@ -0,0 +1,13 @@
+const City = function (name, mista, quantity, materuk) { 
+     this.name = name; 
+     this.mista = mista; 
+     this.quantity = quantity;
+      this.materuk = materuk;
+     this.SayQuantity = function ()
+      { console.log("in this city live" + " " + this.quantity + " " + "people");
+     };
+    };
+     const london = new City("London", 250, 111111, "europa");
+     const lviv = new City("Lviv", 300, 22222222 , "europa");
+     const odessa = new City("Odessa", 56, 33333333, "europa");
+london.SayQuantity();lviv.SayQuantity();odessa.SayQuantity();
